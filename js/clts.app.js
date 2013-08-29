33f9310aeeb72ede91f03b80cec88d28c38c493a
Peter Pistorius <peter.pistorius@gmail.com>
2013-08-29 17:36:23 +0100

    remove js library deps.

--- js/clts.app.js
+++ js/clts.app.js
@@ -1,3 +1,6 @@
+window.api_host = 'http://localhost:8000/api/v1/';
+
+
 var app = angular.module('cltsApp', []);
 
 
@@ -8,28 +11,62 @@ app.run(function($rootScope) {
 // data service factory is a singleton that's only concerned
 // with keeping the API's data up to date.
 app.factory('dataService', ['$http', function($http) {
-    var d = {};
-    d.champion = {};
-    d.champion.activate = function(msisdn) {
-        // activate the user via the api
-        console.log('activate', msisdn);
+
+    // TODO, we need a better API/ Data service here.
+
+    
+    var clts = localStorage.getItem('clts.v1') || false;
+    window.clts = clts;
+    // first run, setup default data and mark the champion as
+    // inactive.
+    if (!clts) {
+        clts = {
+            champion: {
+                msisdn: '1234567890',
+                activated: false
+            },
+            villages: {
+            }
+        };
+    }
+
+
+    var saveData = function() {
+        
     };
 
-    //var villages = localStorage.getItem('villages') || {};
-    d.villages = {};
-    d.villages.update = function() {
-        console.log('fetch a new list of villages');
+    var activateChampion = function(msisdn) {
+
+        var url = window.api_host + 'champions/' + msisdn +  '/activate/';
+        $http.post(url, {})
+            .success(function(data, status, headers, config) {
+
+                clts.champion.activated = true;
+                clts.villages = data;
+                saveData();
+            })
+            .error(function(data, status, headers, config) {
+                // whoopsie.
+            });
     };
 
-    return d;
+    var fetchVillages = function() {
+
+    };
+
+    return {
+        data: clts,
+        activateChampion: activateChampion,
+        fetchVillages: fetchVillages
+    };
 }]);
 
 // the activation controller...
 app.controller('ActivationController', ['$scope', 'dataService',
-
     function($scope, dataService) {
-        $scope.msisdn = '1234567890'; // example
+        $scope.champion = dataService.data.champion;
         $scope.activate = function(msisdn) {
-            dataService.champion.activate(msisdn);
+            dataService.activateChampion(msisdn);
         };
-}]);
+    }
+]);
